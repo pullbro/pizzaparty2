08135266dfaa76165b9ded87379ce5812de3bdd3
pullbro <lp3749@rit.edu>
2026-02-17 14:29:40 -0500

    removed button and made the recompute happen onclick

--- PizzaParty4/app/src/main/java/com/example/pizzaparty3/ui/PizzaPartyScreen.kt
+++ PizzaParty4/app/src/main/java/com/example/pizzaparty3/ui/PizzaPartyScreen.kt
@@ -5,7 +5,6 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -16,7 +15,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pizzaparty3.ui.theme.PizzaParty3Theme
-
 @Composable
 fun NumPeopleField(onValueChange: (String) -> Unit, numPeople: String = "", modifier: Modifier = Modifier) {
     Toast.makeText(LocalContext.current, "NumPeopleField recomposed", Toast.LENGTH_SHORT).show()
@@ -27,48 +25,38 @@ fun NumPeopleField(onValueChange: (String) -> Unit, numPeople: String = "", modi
         onValueChange = onValueChange,
         modifier = modifier
     )
-
 }
 
 @Composable
 fun PizzaPartyScreen(pvm: PizzaPartyViewModel, modifier: Modifier = Modifier) {
-
-//    var numPeople by remember { mutableStateOf("") }
-
     Column {
         Text(
             text = "Pizza Party",
             modifier = modifier
         )
-
-//    var numPeople by remember { mutableStateOf("") }
-//    var numPeople = ""
-        NumPeopleField({Log.d("MainActivity", it)
-            pvm.numPeopleInput = it }, numPeople = pvm.numPeopleInput
-        )
-
-//    var selectedOption by remember { mutableStateOf("Light") }
-        //val isOptionSelected: (String)->Boolean = {pvm.selectedOption == it}
-
-        RadioButtonGroup({ pvm.selectedOption = "Light" }, onClickMedium = { pvm.selectedOption = "Medium" }, onClickRavenous = { pvm.selectedOption = "Ravenous" }, selectedOption = pvm.selectedOption, )
-
-
-        //   var totalPizzas =0
-        // var totalPizzas  by remember {  mutableIntStateOf(0) }
-
-        Text(text = "Total pizzas: ${pvm.totalPizzas}")
-
-        val context = LocalContext.current
-        Button(onClick = {
-            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        NumPeopleField({
+            Log.d("MainActivity", it)
+            pvm.numPeopleInput = it
             pvm.calculateNumPizzas()
-        }){
-            Text(text = "Calculate")
-        }
-
-
+        }, numPeople = pvm.numPeopleInput
+        )
+        RadioButtonGroup(
+            {
+                pvm.selectedOption = "Light"
+                pvm.calculateNumPizzas()
+            },
+            onClickMedium = {
+                pvm.selectedOption = "Medium"
+                pvm.calculateNumPizzas()
+            },
+            onClickRavenous = {
+                pvm.selectedOption = "Ravenous"
+                pvm.calculateNumPizzas()
+            },
+            selectedOption = pvm.selectedOption,
+        )
+        Text(text = "Total pizzas: ${pvm.totalPizzas}")
     }
-
 }
 
 @Composable
