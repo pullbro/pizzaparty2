cd8c0c911cfe40e286bc773cdb25c462642f71b6
pullbro <lp3749@rit.edu>
2026-02-25 12:55:08 -0500

    added colors for dark mode (red)

--- PizzaParty4/app/src/main/java/com/example/pizzaparty3/ui/theme/Color.kt
+++ PizzaParty4/app/src/main/java/com/example/pizzaparty3/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.pizzaparty3.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkPrimary = Color(0xFFFF0000)
+val LightPrimary = Color(0xFF0000FF)
+val DarkBackground = Color(0xFF120D16)
+val DarkSurface = Color(0xFF120D16)
+val LightBackground = Color(0xFFF7F3FA)
+val LightSurface = Color(0xFFF7F3FA)
