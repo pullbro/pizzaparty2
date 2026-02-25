1a9ee1df0f48becfaa91dae08ded96c65ccb2c4b
pullbro <lp3749@rit.edu>
2026-02-25 13:15:01 -0500

    finished theme.kt file

--- PizzaParty4/app/src/main/java/com/example/pizzaparty3/ui/theme/Theme.kt
+++ PizzaParty4/app/src/main/java/com/example/pizzaparty3/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.pizzaparty3.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,35 +8,35 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimary,
+    secondary = DarkPrimary,
+    tertiary = DarkPrimary,
+    onPrimary = Color.White,
+    background = DarkBackground,
+    surface = DarkSurface,
+    onBackground = Color.White,
+    onSurface = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = LightPrimary,
+    secondary = LightPrimary,
+    tertiary = LightPrimary,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = LightBackground,
+    surface = LightSurface,
+    onBackground = Color.Black,
+    onSurface = Color.Black
 )
 
 @Composable
 fun PizzaParty3Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -45,7 +44,6 @@ fun PizzaParty3Theme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
